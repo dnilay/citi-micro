0bc65fab572678649565627bd5c0ea8589a02b23
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-08 17:49:23 +0530

    Add files via upload

--- my-second-project/src/Main.java
+++ my-second-project/src/Main.java
@@ -1,13 +1,20 @@
 import com.domain.Color;
+import com.domain.Customer;
+import com.domain.CustomerType;
 import com.domain.Size;
 import com.example.Shirt;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
-        Shirt shirt=new Shirt();
+      /*  Shirt shirt=new Shirt();
         shirt.createShirt(UUID.randomUUID().toString(), Color.BLUE, null);
-        System.out.println(shirt.getShirtDetails());
+        System.out.println(shirt.getShirtDetails());*/
+
+        Customer customer=new Customer();
+        customer.createCustomer(new Random().nextInt(),"John","Doe","john@email.com", CustomerType.REGULAR_CUSTOMER);
+        System.out.println(customer.getDetails());
     }
 }
--- my-second-project/src/com/domain/Customer.java
+++ my-second-project/src/com/domain/Customer.java
@@ -9,4 +9,18 @@ public class Customer {
     private String email;
 
     private CustomerType customerType;
+
+    public void createCustomer(int id,String fName,String lName,String e,CustomerType cType)
+    {
+        customerId=id;
+        firstName=fName;
+        lastName=lName;
+        email=e;
+        customerType=cType;
+    }
+
+    public String getDetails()
+    {
+        return "Customer ID: "+customerId+" First Name: "+firstName+" Last Name: "+lastName+" email: "+email+" cuomer type: "+customerType;
+    }
 }
