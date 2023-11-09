074935433e0c2bbddc8b1c043706a66edd92edc9
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-09 16:34:03 +0530

    Add files via upload

---
+++ array_demo1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+
+
+    public static void main(String[] args)
+
+    {
+      int a[];
+      a=new int[10];
+
+      for(int i=0;i<10;i++)
+      {
+          a[i]=i+1;
+      }
+
+      for(int i=0;i<10;i++)
+      {
+          System.out.println(a[i]);
+      }
+
+
+
+
+
+    }
+}
