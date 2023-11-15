476afb01616ad38f4feed8b85ac18633b5e315af
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-15 17:36:02 +0530

    Add files via upload

---
+++ project2-15_11_23/src/com/example/demo/Employee.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+// Employee.java
+// Child class Employee
+public class Employee extends Person {
+    private int employeeId;
+    private String jobTitle;
+
+    public Employee(String firstName, String lastName, int employeeId, String jobTitle) {
+        super(firstName, lastName);
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName() + ", " + jobTitle;
+    }
+}
---
+++ project2-15_11_23/src/com/example/demo/Person.java
@@ -0,0 +1,20 @@
+package com.example.demo;// Person.java
+// Parent class Person
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
