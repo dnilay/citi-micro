48e58f2e3912ead595d1955cbea967d4e95f962f
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-15 17:18:36 +0530

    Add files via upload

---
+++ project2-15_11_23/src/com/example/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.example;
+
+// SavingsAccount.java
+// Child class SavingsAccount
+public class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() - amount < 100) {
+            System.out.println("Minimum balance of $100 required!");
+        } else {
+            super.withdraw(amount);
+        }
+    }
+}
