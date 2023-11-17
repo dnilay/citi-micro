0b9c52fcd314d38117ecce13b79687b7c6bc5bda
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-17 15:48:02 +0530

    Add files via upload

--- project-17_11_23/src/Main.java
+++ project-17_11_23/src/Main.java
@@ -1,14 +1,11 @@
 import com.example.Customer;
 import com.example.Employee;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Employee> set=new HashSet<Employee>();
+        /*Set<Employee> set=new HashSet<Employee>();
 
         set.add(new Employee(100,"John Doe",12000));
         set.add(new Employee(101,"Mary Public",12000));
@@ -19,6 +16,24 @@ public class Main {
         {
             Employee e=i.next();
             System.out.println(e);
+        }
+*/
+        Set<Integer> set=new HashSet<Integer>();
+        set.add(100);
+        set.add(200);
+        set.add(300);
+        set.add(100);
+        System.out.println(set);
+        List<Integer> list=new LinkedList<Integer>();
+        list.add(100);
+        list.add(2);
+        list.add(300);
+        list.add(100);
+        System.out.println(list);
+        Iterator<Integer> iterator=list.iterator();
+        while (iterator.hasNext())
+        {
+            System.out.println(iterator.next());
         }
 
     }
