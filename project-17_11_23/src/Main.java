f56bdeb2c548766099c7b2549ccd84e43b125c29
Nilay <54245048+dnilay@users.noreply.github.com>
2023-11-17 14:54:55 +0530

    Add files via upload

--- project-17_11_23/src/Main.java
+++ project-17_11_23/src/Main.java
@@ -1,3 +1,6 @@
+import com.example.Customer;
+import com.example.Employee;
+
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -10,27 +13,49 @@ public class Main {
         int a=1;
         //wrapper type
         //auto boxing
-        Integer b=a;
+        Integer b= a;
         //auto unboxing
         int result=b;
         char c='a';
         Character c1=new Character('z');
-
+        set.add(new Customer());
         set.add(10);
         set.add("abcd");
         set.add(new Date());
         set.add(10);
         set.add(100);
+        set.add(new Employee(100,"John Doe",12000));
         //using iterator
         Iterator i=set.iterator();
         while (i.hasNext())
         {
-            System.out.println(i.next());
+            Object o=i.next();
+            if(o instanceof Integer)
+            {
+                int x=(Integer)o;
+                System.out.println(x);
+            } else if (o instanceof Character) {
+                char ch=(Character)o;
+                System.out.println(ch);
+            }
+            else if (o instanceof Date)
+            {
+                Date d=(Date) o;
+                System.out.println(d);
+            }
+            else if(o instanceof Employee)
+            {
+                Employee e=(Employee) o;
+                System.out.println(e);
+            } else if (o instanceof String) {
+                String s=(String) o;
+                System.out.println(s);
+
+            } else
+            {
+                System.out.println("can not determine the type");
+            }
         }
-        //using enhanched for loop
-        for(Object o:set)
-        {
-            System.out.println(o);
-        }
+
     }
 }
